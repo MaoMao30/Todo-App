4785fe3d2a768bd5d1992a2cfb3b567bda5a4cbc
MaoMao30 <128595572+MaoMao30@users.noreply.github.com>
2025-01-06 14:51:28 +0200

    Initial commit: Todo App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoAlpha } from './components/TodoAlpha';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoAlpha  />
+    </div>
+  );
+}
+
+export default App;
